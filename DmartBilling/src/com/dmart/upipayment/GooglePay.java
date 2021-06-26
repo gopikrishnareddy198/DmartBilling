6841cfcb3ef98dada59509d87568fd8f83d2fe8e
Developer <Developer98@gmail.com>
2021-06-26 20:06:57 +0530

    Addded new method again to GooglePay

--- DmartBilling/src/com/dmart/upipayment/GooglePay.java
+++ DmartBilling/src/com/dmart/upipayment/GooglePay.java
@@ -3,8 +3,11 @@ package com.dmart.upipayment;
 public class GooglePay {
 	
 	private void getReferenceNumber() {
+		
+	}
+
+	private void getUpiNumber() {
 		// TODO Auto-generated method stub
 
 	}
-
 }
